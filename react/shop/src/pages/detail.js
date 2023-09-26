2fc52097e86d4c3826a9bd1b434d9716ba2c2a6c
cjlotto <joung6517@gamil.com>
2023-09-26 15:19:42 +0900

    useEffect

--- react/shop/src/pages/detail.js
+++ react/shop/src/pages/detail.js
@@ -1,14 +1,30 @@
 import { useParams } from "react-router-dom"
+import { useEffect, useState } from "react";
 
 function Detail(props) {
     let { id } = useParams();
     let shoe = props.shoes.filter((shoe) => {
         return shoe.id == id; 
     })[0]
+    let [alert, setAlert] = useState(true);
     id = parseInt(id);
+
+    useEffect(() => {
+        let timer = setTimeout(() => {
+                        setAlert(false);
+                    }, 2000);
+
+        return () => {
+            /* code */
+            clearTimeout(timer);
+        }
+    }, [])
 
     return (
         <div className="container">
+            {
+                alert ? <div className="alert alert-warning">2초 후 사라진다.</div> : null
+            }
             <div className="row">
                 <div className="col-md-6">
                 <img src={`https://codingapple1.github.io/shop/shoes${shoe.id+1}.jpg`} width="100%" />
