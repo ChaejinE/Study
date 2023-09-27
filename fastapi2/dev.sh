4bf6376dd2499f9aabb01f99c5f413fb9d29c6c0
cjlotto <joung6517@gamil.com>
2023-09-27 18:13:47 +0900

    setup

--- fastapi2/dev.sh
+++ fastapi2/dev.sh
@@ -2,4 +2,4 @@
 APP_NAME=fastapi-study
 
 docker build -f Dockerfile.dev -t ${APP_NAME}:latest .
-docker run --rm -p 8000:8000 -v .:/app ${APP_NAME}
+docker run --rm -p 8000:8000 -v .:/fastapi ${APP_NAME}
