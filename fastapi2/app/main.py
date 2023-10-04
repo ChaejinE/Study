d2aa44d199ce2c81686dcaea250b42bec461c9be
cjlotto <joung6517@gamil.com>
2023-10-04 11:49:20 +0900

    add conf

--- fastapi2/app/main.py
+++ fastapi2/app/main.py
@@ -1,8 +1,14 @@
 from fastapi import FastAPI
-
+from app.common.config import conf
 import uvicorn
 
-app = FastAPI()
+def create_app():
+    c = conf()
+    app = FastAPI()
+    
+    return app
+    
+app = create_app()
 
 if __name__ == "__main__":
     uvicorn.run("main:app", host="0.0.0.0", port=8080, reload=True)
