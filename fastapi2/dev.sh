a6179c8e6767a746c947d7ebc1a2cf3200ca8c75
cjlotto <joung6517@gamil.com>
2023-10-04 17:42:00 +0900

    only run fastapi app

--- fastapi2/dev.sh
+++ fastapi2/dev.sh
@@ -1,5 +1,4 @@
 #/bin/bash
 APP_NAME=fastapi-study
-
 docker build -f Dockerfile.dev -t ${APP_NAME}:latest .
-docker run --rm -p 8000:8000 -v .:/fastapi ${APP_NAME}
+docker run --rm -p 8000:8000 -v ./app:/usr/src/app ${APP_NAME}
