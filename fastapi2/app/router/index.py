e1d76009bdc796c2fe681beef657d4a534229f60
cjlotto <joung6517@gamil.com>
2023-10-07 17:10:06 +0900

    health check 용 API

--- fastapi2/app/router/index.py
+++ fastapi2/app/router/index.py
@@ -13,10 +13,5 @@ router = APIRouter()
 
 @router.get("/")
 async def index(session: AsyncSession = Depends(get_async_session)):
-    user = User()
-    user.name = "코알라"
-    session.add(user)
-    await session.commit()
-    await session.refresh(user)
     current_time = datetime.utcnow()
     return Response(f"UTC: {current_time.strftime('%Y.%m.%d %H:%M:%S')})")
