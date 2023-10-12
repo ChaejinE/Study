5de78d32520496dc16a8d3b780ff6ebcd424ba68
cjlotto <joung6517@gamil.com>
2023-10-12 22:51:18 +0900

    async로 인한 동작안되는 문제해결로 인한 구조 변경

--- fastapi2/app/router/users.py
+++ fastapi2/app/router/users.py
@@ -1,7 +1,12 @@
-from fastapi import APIRouter
+from fastapi import APIRouter, Depends
 
 from starlette.requests import Request
+from starlette.responses import Response
 
+from sqlalchemy import select
+from sqlalchemy.ext.asyncio import AsyncSession
+
+from database.conn import get_async_session
 from database.schema.users import User
 from model.users import UserMe
 
@@ -9,8 +14,22 @@ router = APIRouter()
 
 
 @router.get("/me", response_model=UserMe)
-async def get_user(request: Request):
+async def get_user(
+    request: Request, session: AsyncSession = Depends(get_async_session)
+):
     user = request.state.user
-    user_info = User.get(id=user.id)
+    query = select(User).limit(10)
+    col = getattr(User, "id")
+    query = query.filter(col == user.id)
+    query_result = await session.execute(query)
+    user_info = query_result.scalars().all()
+    if len(user_info) > 1:
+        raise Exception("It must be only one user")
+    elif user_info:
+        user_info = UserMe.model_validate(
+            user_info[0], from_attributes=True
+        ).model_dump()
+    else:
+        return Response(status_code=400, content="User Not Found")
 
     return user_info
