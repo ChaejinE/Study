0e548fdef9216c8a0661cfe4db707428a2d564a5
ChaejinE <joung6517@gmail.com>
2024-01-23 22:17:17 +0900

    add return type

--- FastAPI/i_response_model/main.py
+++ FastAPI/i_response_model/main.py
@@ -14,22 +14,66 @@ FastAPI will use this return type to
 """
 
 
+class BaseUser(BaseModel):
+    username: str
+    email: EmailStr
+    full_name: Union[str, None] = None
+
+
 class UserIn(BaseModel):
-    username: str
+    # It inhertis from BaseUser. so it will include all the fields from both models
     password: str
-    email: EmailStr
-    full_name: Union[str, None] = None
 
 
-class UserOut(BaseModel):
-    username: str
-    email: EmailStr
-    full_name: Union[str, None] = None
-
-
-@app.post("/user/", response_model=UserOut)
+# Password is in a input, but is not in a output becuase of response_model
+@app.post("/user/", response_model=BaseUser)
 async def create_user(user: UserIn) -> Any:
     return user
+
+
+from fastapi import Response
+from fastapi.responses import JSONResponse, RedirectResponse
+
+
+# We can return a response directly
+# Responses is subclass of Response. so they are handled by fastapi
+@app.post("/protal")
+def get_portal(teleport: bool = False) -> Response:
+    if teleport:
+        return RedirectResponse(url="https://www.youtube.com/watch?v=dQw4w9WgXcQ")
+    return JSONResponse(content={"message": "Here's your interdimentional protal"})
+
+
+# Return type is invalid pydantic type so without reponse_model=None, it is fail
+# So, add response_model=None
+# It don't apply to valid data by pydantic
+@app.post("/protal2", response_model=None)
+def get_portal(teleport: bool = False) -> Union[Response, dict]:
+    if teleport:
+        return RedirectResponse(url="https://www.youtube.com/watch?v=dQw4w9WgXcQ")
+    return {"message": "Here's your interdimensional portal."}
+
+
+class Item(BaseModel):
+    name: str
+    description: Union[str, None] = None
+    price: float
+    tax: float = 10.5
+    tags: list[str] = []
+
+
+items = {
+    "foo": {"name": "Foo", "price": 50.2},
+    "bar": {"name": "Bar", "description": "The bartenders", "price": 62, "tax": 20.2},
+    "baz": {"name": "Baz", "description": None, "price": 50.2, "tax": 10.5, "tags": []},
+}
+
+
+# If response_model_exclude_unset set True, it makes not pass default value
+# We can use response_model_exclude, response_model_include
+@app.get("/items/{item_id}", response_model=Item, response_model_exclude_unset=True)
+async def read_item(item_id: str):
+    return items[item_id]
 
 
 if __name__ == "__main__":
