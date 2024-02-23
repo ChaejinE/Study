cd907268f0b619f61613a25c06499de6b3a43091
ChaejinE <joung6517@gmail.com>
2024-02-23 19:50:17 +0900

    update

---
+++ terraform/helloworld/README.md
@@ -0,0 +1,19 @@
+```bash
+terraform init
+```
+- Initialization
+
+```bash
+terraform plan
+```
+- Check values will be applied
+
+```bash
+terraform apply
+```
+- Apply
+
+```bash
+terraform destroy
+```
+- Remove
--- terraform/helloworld/main.tf
+++ terraform/helloworld/main.tf
@@ -1,3 +1,14 @@
+terraform {
+  required_providers {
+    local = {
+      source  = "hashicorp/local" # Provider
+      version = "2.4.0" # Provider version
+    }
+  }
+
+  required_version = ">= 1.4"
+}
+
 resource "local_file" "demo" {
     content = "hi. how are you ?"
     filename = "helloworld.txt"
