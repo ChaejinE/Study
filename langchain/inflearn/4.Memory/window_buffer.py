0a2784f7ab1d800407de5ce57401668c49257389
ChaejinE <joung6517@gmail.com>
2024-03-01 23:55:18 +0900

    add result

--- langchain/inflearn/4.Memory/window_buffer.py
+++ langchain/inflearn/4.Memory/window_buffer.py
@@ -5,6 +5,7 @@ memory.save_context({"input": "hi"}, {"output": "whats up"})
 memory.save_context({"input": "not much you"}, {"output": "not much"})
 result = memory.load_memory_variables(dict())
 print(f"1. Window memory : {result}")
+# 1. Window memory : {'history': 'Human: not much you\nAI: not much'}
 
 # memory = ConversationBufferWindowMemory(
 #     k=1, return_messages=True
@@ -12,3 +13,4 @@ print(f"1. Window memory : {result}")
 memory.return_messages = True
 result = memory.load_memory_variables(dict())
 print(f"2. Window memory when return messages is True : {result}")
+# 2. Window memory when return messages is True : {'history': [HumanMessage(content='not much you'), AIMessage(content='not much')]}
