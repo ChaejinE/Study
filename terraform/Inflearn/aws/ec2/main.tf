76e6ef92520f3115104f10d0a6416cc65f9e0b99
ChaejinE <joung6517@gmail.com>
2024-03-05 17:42:05 +0900

    add ec2

--- terraform/Inflearn/aws/ec2/main.tf
+++ terraform/Inflearn/aws/ec2/main.tf
@@ -1,17 +1,38 @@
 provider "aws" {
-    region = "us-west-2"
+  region = "us-west-2"
 }
 
 data "aws_ami" "ubuntu" {
   most_recent = true
 
   filter {
-    name = "name"
-    values = ["ubuntu/images/hvm-ssd/ubuntu-focal-20.04-amd64-server-*"]
+    name   = "name"
+    values = ["ubuntu/images/hvm-ssd/ubuntu-jammy-22.04-amd64-server-20240207.1"]
   }
 
   filter {
-    name = "virtualization-type"
+    name   = "virtualization-type"
     values = ["hvm"]
+  }
+
+  owners = ["099720109477"]
+}
+
+resource "aws_instance" "web" {
+  ami           = data.aws_ami.ubuntu.id
+  instance_type = "t2.micro"
+
+  tags = {
+    name = "MyEc2"
+  }
+
+  depends_on = [aws_default_subnet.default_az1]
+}
+
+resource "aws_default_subnet" "default_az1" {
+  availability_zone = "us-west-2a"
+
+  tags = {
+    Name = "Default subnet for us-west-2a"
   }
 }
