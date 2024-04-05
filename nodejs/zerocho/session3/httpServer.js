1302a7b2d816888c8188d07e396fa65ce179723f
ChaejinE <joung6517@gmail.com>
2024-04-06 00:32:17 +0900

    add server

--- nodejs/zerocho/session3/httpServer.js
+++ nodejs/zerocho/session3/httpServer.js
@@ -1,10 +1,16 @@
 const http = require("http");
 
-http.createServer((req, res) => {
+const server = http.createServer((req, res) => {
     res.write("<h1>Hello</h1>");
     res.write("<h2>Hello</h2>");
     res.end("<p>lotto</p>");
 })
-.listen(8080, () => {
-    console.log("8080 port")
+    .listen(8080);
+
+server.on("listening", () => {
+    console.log("8080 port !!!!!")
+})
+
+server.on("error", (err) => {
+    console.error(err);
 })
