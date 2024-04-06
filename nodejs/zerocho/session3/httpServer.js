1fa5b9f50dcc1f5de879c14bef783281840b5cb6
ChaejinE <joung6517@gmail.com>
2024-04-06 11:12:45 +0900

    add server

--- nodejs/zerocho/session3/httpServer.js
+++ nodejs/zerocho/session3/httpServer.js
@@ -1,9 +1,19 @@
 const http = require("http");
+const fs = require("fs").promises;
 
-const server = http.createServer((req, res) => {
-    res.write("<h1>Hello</h1>");
-    res.write("<h2>Hello</h2>");
-    res.end("<p>lotto</p>");
+const server = http.createServer(async (req, res) => {
+    try {
+        res.writeHead(200, {"Content-Type": "text/html; charset=utf-8"});
+        // res.write("<h1>Hello</h1>");
+        // res.write("<h2>Hello</h2>");
+        // res.end("<p>lotto</p>");
+        const data = await fs.readFile("./server.html");
+        res.end(data);
+    } catch (err) {
+        res.writeHead(200, {"Content-Type": "text/html; charset=utf-8"});
+        console.error(err);
+        res.end(err.message);
+    }
 })
     .listen(8080);
 
