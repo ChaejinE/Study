0b531aa84855d664de11db2773bc64a5e3be3462
ChaejinE <joung6517@gmail.com>
2024-04-27 18:54:17 +0900

    update

--- nodejs/zerocho/api_caller/controllers/index.js
+++ nodejs/zerocho/api_caller/controllers/index.js
@@ -1,5 +1,4 @@
 const axios = require("axios");
-const { token } = require("morgan");
 
 exports.test = async (req, res, next) => {
     try {
--- nodejs/zerocho/api_server/controllers/v1.js
+++ nodejs/zerocho/api_server/controllers/v1.js
@@ -1,47 +1,43 @@
-const {Domain, User} = require("../models");
-const jwt = require("jsonwebtoken");
+const jwt = require('jsonwebtoken');
+const { Domain, User, Post, Hashtag } = require('../models');
 
 exports.createToken = async (req, res) => {
-    const { clientSecret } = req.body;
-    try {
-        console.log("CS : ", clientSecret);
-        const domain = Domain.findOne({
-            where: { clientSecret: clientSecret },
-            include: {
-                model: User,
-                attributes: ["id", "nick"],
-            }
-        });
-
-        if (!domain.Domain || !domain.User) {
-            return res.status(401).json({
-                code: 401,
-                message: "Not registered domain, please register your domain"
-            })
-        }
-
-        const token = jwt.sign({
-            id: domain.User.id,
-            nick: domain.User.nick,
-        }, process.env.JWT_SECRET, {
-            expiresIn:"1m",
-            issuer: "nodebird"
-        });
-
-        return res.json({
-            code: 200,
-            message: "Success to make jwt token",
-            token
-        })
-    } catch (err) {
-        console.error(err);
-        return res.json({
-            code: 500,
-            message: "Server Error"
-        })
+  const { clientSecret } = req.body;
+  try {
+    const domain = await Domain.findOne({
+      where: { clientSecret },
+      include: {
+        model: User,
+        attribute: ['nick', 'id'],
+      },
+    });
+    if (!domain) {
+      return res.status(401).json({
+        code: 401,
+        message: '등록되지 않은 도메인입니다. 먼저 도메인을 등록하세요',
+      });
     }
-}
+    const token = jwt.sign({
+      id: domain.User.id,
+      nick: domain.User.nick,
+    }, process.env.JWT_SECRET, {
+      expiresIn: '1m', // 1분
+      issuer: 'nodebird',
+    });
+    return res.json({
+      code: 200,
+      message: '토큰이 발급되었습니다',
+      token,
+    });
+  } catch (error) {
+    console.error(error);
+    return res.status(500).json({
+      code: 500,
+      message: '서버 에러',
+    });
+  }
+};
 
-exports.tokenTest = async (req, res) => {
-    res.json(res.locals.decoded);
-}
+exports.tokenTest = (req, res) => {
+  res.json(res.locals.decoded);
+};
