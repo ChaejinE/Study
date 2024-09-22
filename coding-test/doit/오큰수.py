d831f8e28515654506135743deedf7580dbe66ea
ChaejinE <joung6517@gmail.com>
2024-09-22 17:34:49 +0900

    update

--- coding-test/doit/오큰수.py
+++ coding-test/doit/오큰수.py
@@ -8,12 +8,12 @@ result = [0] * N
 
 for i in range(N):
     while stk and A[stk[-1]] < A[i]:
-        result[stk.pop()] = str(A[i])
+        result[stk.pop()] = A[i]
 
     stk.append(i)
 
 while stk:
-    result[stk.pop()] = str(-1)
+    result[stk.pop()] = -1
 
 for elem in result:
     print(elem, end=" ")
