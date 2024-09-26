9f895ce28f8d06a2a1ea3ec347ac878738ab2b79
reconlabs-luke <luke@reconlabs.ai>
2024-09-26 10:36:30 +0900

    업데이트

--- coding-test/doit/퀵정렬.py
+++ coding-test/doit/퀵정렬.py
@@ -9,29 +9,35 @@ def quick_sort(L):
     global N
 
     if len(L) <= 1:
-        return []
+        return L
 
     pivot_idx = len(L) - 1
     l_idx = 0
-    r_idx = len(L) - 2
-    while l_idx < r_idx:
+    r_idx = pivot_idx - 1
+
+    # The loop until l_idx and r_idx cross
+    while l_idx <= r_idx:
         # search lower
-        while l_idx < N - 1 and L[l_idx] <= L[pivot_idx]:
+        while l_idx < pivot_idx and L[l_idx] <= L[pivot_idx]:
             l_idx += 1
 
         # search greater
         while r_idx > 0 and L[r_idx] >= L[pivot_idx]:
             r_idx -= 1
 
+        # swap
         if l_idx < r_idx:
-            # swap
             L[l_idx], L[r_idx] = L[r_idx], L[l_idx]
 
         l_idx += 1
         r_idx -= 1
 
+    # Reset the values before adding or substracting one in the last while loop
+    l_idx -= 1
+    r_idx += 1
+
     m = l_idx + 1 if L[l_idx] < L[pivot_idx] else l_idx
-    return quick_sort(L[0:m]) + [L[pivot_idx]] + quick_sort(L[m : N - 1])
+    return quick_sort(L[0:m]) + [L[pivot_idx]] + quick_sort(L[m:pivot_idx])
 
 
 print(quick_sort(A))
