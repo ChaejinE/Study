2978db58cbe05bd6a66f5f5ad2c7c44995e9c422
ChaejinE <joung6517@gmail.com>
2024-11-03 22:49:36 +0900

    Basic theme

--- react/nomad/masterClass/react-masterclass/src/App.js
+++ react/nomad/masterClass/react-masterclass/src/App.js
@@ -1,62 +1,22 @@
-import styled, {keyframes} from "styled-components";
+import styled from "styled-components";
+
+const Title = styled.h1`
+  color: ${(props) => props.theme.textColor};
+`;
 
 const Wrapper = styled.div`
   display: flex;
-`;
-
-const rotationAnimation = keyframes`
-  /* from {
-    transform: rotate(0deg);
-    border-radius: 0px;
-  }
-  to {
-    transform: rotate(360deg);
-    border-radius: 100px;
-  } */
-  0% {
-    transform: rotate(0deg);
-    border-radius: 0px;
-  }
-  50% {
-    transform: rotate(360deg);
-    border-radius: 100px;
-  }
-  100% {
-    transform: rotate(360deg);
-    border-radius: 0px;
-  }
-`;
-
-const Emoji = styled.span`
-  font-size: 36px;
-`;
-
-const Box = styled.div`
-  height: 200px;
-  width: 200px;
-  background-color: tomato;
-  animation: ${rotationAnimation} 1s linear infinite;
-  display: flex;
+  height: 100vh;
+  width: 100vw;
   justify-content: center;
   align-items: center;
-
-  ${Emoji} {
-    &:hover {
-      font-size: 70px;
-    }
-    &:active {
-      opacity: 0;
-    }
-  }
+  background-color: ${props => props.theme.backgroundColor};
 `;
 
 function App() {
   return (
     <Wrapper>
-      <Box>
-        <Emoji>😀</Emoji>
-      </Box>
-      <Emoji>🏆</Emoji>
+      <Title>Hello</Title>
     </Wrapper>
   );
 }
