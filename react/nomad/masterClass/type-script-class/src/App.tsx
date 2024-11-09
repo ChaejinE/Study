c25238c2f5c84bd39f68461a33ee83569dbb19af
ChaejinE <joung6517@gmail.com>
2024-11-09 13:18:30 +0900

    [TypeScript] Recap

--- react/nomad/masterClass/type-script-class/src/App.tsx
+++ react/nomad/masterClass/type-script-class/src/App.tsx
@@ -6,10 +6,25 @@ const Container = styled.div`
 const H1 = styled.h1`
   color: ${props => props.theme.textColor};
 `; 
+
+interface DummyProps {
+  text: string;
+  active?: boolean;
+};
+
+function Dummy({text, active = false}: DummyProps) {
+  return <H1>{text}</H1>;
+}
+
 function App() {
+  // https://reactjs.org/docs/events.html
+  const onClick = (event: React.MouseEvent<HTMLButtonElement>) => {
+  };
+
   return (
     <Container>
-       <H1>protected</H1>
+       <Dummy active text="hello" />
+       <button onClick={onClick}>click me</button>
     </Container>
   );
 }
