07ebe0d1d87d3d524fd811dcf7d2884747e52177
ChaejinE <joung6517@gmail.com>
2024-11-09 23:17:20 +0900

    [ReactRouter] useNavigate

--- react/nomad/masterClass/react-router-dom-class/src/components/Header.tsx
+++ react/nomad/masterClass/react-router-dom-class/src/components/Header.tsx
@@ -1,7 +1,13 @@
-import { Link } from "react-router-dom";
+import { Link, useNavigate } from "react-router-dom";
 
 function Header() {
-    // For using Link, u should put it  in the BrowserRouter
+  const navigate = useNavigate();
+  const onAboutClick = () => {
+    navigate("/about");
+  };
+
+  // For using Link, u should put it  in the BrowserRouter
+  // Diff between Link and useNavigate : https://velog.io/@ahn-sujin/React-Link-%EC%BB%B4%ED%8F%AC%EB%84%8C%ED%8A%B8%EC%99%80-useNavigate%EC%9D%98-%EC%B0%A8%EC%9D%B4
   return ( 
     <header>
       <ul>
@@ -9,7 +15,7 @@ function Header() {
           <Link to={"/"}>Home</Link>
         </li>
         <li>
-          <Link to={"/about"}>About</Link>
+          <button onClick={onAboutClick}>About</button>
         </li>
       </ul> 
     </header>
