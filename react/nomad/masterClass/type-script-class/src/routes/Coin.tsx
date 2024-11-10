4bec6392dec92790cea843978040f97f929c1d1d
ChaejinE <joung6517@gmail.com>
2024-11-10 18:55:57 +0900

    [CryptoTracker] Coin Data

--- react/nomad/masterClass/type-script-class/src/routes/Coin.tsx
+++ react/nomad/masterClass/type-script-class/src/routes/Coin.tsx
@@ -1,4 +1,4 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import { useLocation, useParams } from "react-router-dom";
 import styled from "styled-components";
 
@@ -25,13 +25,35 @@ const Loader = styled.span`
   display: block;
 `;
 
+interface CoindParams {
+  coinId: string;
+}
+
 interface ICoindState {
   name: string;
 }
 
 function Coin() {
   const [loading, setLoading] = useState(true);
+  const { coinId } = useParams<CoindParams>();
   const { state } = useLocation<ICoindState>();
+  const [info, setInfo] = useState({});
+  const [priceInfo, setPriceInfo] = useState({});
+
+  useEffect(() => {
+    (async() => {
+      const infoData = await (
+        await fetch(`https://api.coinpaprika.com/v1/coins/${coinId}`)
+      ).json(); // making oneline
+      const priceData = await(
+        await fetch(`https://api.coinpaprika.com/v1/tickers/${coinId}`)
+      ).json();
+      console.log(infoData);
+      console.log(priceData)
+      setInfo(infoData);
+      setPriceInfo(priceData);
+    })();
+  }, []);
 
   return (
     <Container>
