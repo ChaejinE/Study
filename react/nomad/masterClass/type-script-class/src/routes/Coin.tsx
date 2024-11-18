a5bc98d453476fa9d2f188f5c5b59aabea02d900
ChaejinE <joung6517@gmail.com>
2024-11-18 20:59:38 +0900

    [CryptoTracker] Nested Routes

--- react/nomad/masterClass/type-script-class/src/routes/Coin.tsx
+++ react/nomad/masterClass/type-script-class/src/routes/Coin.tsx
@@ -1,6 +1,19 @@
 import { useEffect, useState } from "react";
-import { useLocation, useParams } from "react-router-dom";
+import { Switch, Route, useLocation, useParams, useRouteMatch } from "react-router";
 import styled from "styled-components";
+import Chart from "./Chart";
+import Price from "./Price";
+import { Link } from "react-router-dom";
+
+const Title = styled.h1`
+  font-size: 48px;
+  color: ${(props) => props.theme.accentColor};
+`;
+
+const Loader = styled.span`
+  text-align: center;
+  display: block;
+`;
 
 const Container = styled.div`
   padding: 0px 20px;
@@ -9,31 +22,61 @@ const Container = styled.div`
 `;
 
 const Header = styled.header`
-  height: 10vh;
+  height: 15vh;
   display: flex;
   justify-content: center;
   align-items: center;
 `;
 
-const Title = styled.h1`
-  font-size: 48px;
-  color: ${props => props.theme.accentColor};
+const Overview = styled.div`
+  display: flex;
+  justify-content: space-between;
+  background-color: rgba(0, 0, 0, 0.5);
+  padding: 10px 20px;
+  border-radius: 10px;
 `;
+const OverviewItem = styled.div`
+  display: flex;
+  flex-direction: column;
+  align-items: center;
 
-const Loader = styled.span`
+  span:first-child {
+    font-size: 10px;
+    font-weight: 400;
+    text-transform: uppercase;
+    margin-bottom: 5px;
+  }
+`;
+const Description = styled.p`
+  margin: 20px 0px;
+`;
+const Tabs = styled.div`
+  display: grid;
+  grid-template-columns: repeat(2, 1fr);
+  margin: 25px 0px;
+  gap: 10px;
+`;
+const Tab = styled.span<{ isActive: boolean }>`
   text-align: center;
-  display: block;
+  text-transform: uppercase;
+  font-size: 12px;
+  font-weight: 400;
+  background-color: rgba(0, 0, 0, 0.5);
+  padding: 7px 0px;
+  border-radius: 10px;
+  color: ${props => props.isActive ? props.theme.accentColor : props.theme.textColor};
+  a {
+    display: block ;
+  }
 `;
 
-interface CoindParams {
+interface RouteParams {
   coinId: string;
 }
-
-interface ICoindState {
+interface RouteState {
   name: string;
 }
-
-interface IInfoData {
+interface InfoData {
   id: string;
   name: string;
   symbol: string;
@@ -53,7 +96,7 @@ interface IInfoData {
   first_data_at: string;
   last_data_at: string;
 }
-interface IPriceData {
+interface PriceData {
   id: string;
   name: string;
   symbol: string;
@@ -89,37 +132,82 @@ interface IPriceData {
 
 function Coin() {
   const [loading, setLoading] = useState(true);
-  const { coinId } = useParams<CoindParams>();
-  const { state } = useLocation<ICoindState>();
-  const [info, setInfo] = useState<IInfoData>();
-  const [priceInfo, setPriceInfo] = useState<IPriceData>();
-
+  const { coinId } = useParams<RouteParams>();
+  const { state } = useLocation<RouteState>();
+  const [info, setInfo] = useState<InfoData>();
+  const [priceInfo, setPriceInfo] = useState<PriceData>();
+  const priceMatch = useRouteMatch("/:coinId/price");
+  const chartMatch = useRouteMatch("/:coinId/chart");
   useEffect(() => {
-    (async() => {
+    (async () => {
       const infoData = await (
         await fetch(`https://api.coinpaprika.com/v1/coins/${coinId}`)
-      ).json(); // making oneline
-      const priceData = await(
+      ).json();
+      const priceData = await (
         await fetch(`https://api.coinpaprika.com/v1/tickers/${coinId}`)
       ).json();
-      console.log(infoData);
-      console.log(priceData)
       setInfo(infoData);
       setPriceInfo(priceData);
       setLoading(false);
     })();
-  }, []);
-
+  }, [coinId]);
   return (
     <Container>
       <Header>
-        <Title>{state?.name || "Loading.."}</Title>  
+        <Title>
+          {state?.name ? state.name : loading ? "Loading..." : info?.name}
+        </Title>
       </Header>
-      {loading ? 
-        <Loader>Loading...</Loader> : priceInfo?.quotes.USD.price
-      }
+      {loading ? (
+        <Loader>Loading...</Loader>
+      ) : (
+        <>
+          <Overview>
+            <OverviewItem>
+              <span>Rank:</span>
+              <span>{info?.rank}</span>
+            </OverviewItem>
+            <OverviewItem>
+              <span>Symbol:</span>
+              <span>${info?.symbol}</span>
+            </OverviewItem>
+            <OverviewItem>
+              <span>Open Source:</span>
+              <span>{info?.open_source ? "Yes" : "No"}</span>
+            </OverviewItem>
+          </Overview>
+          <Description>{info?.description}</Description>
+          <Overview>
+            <OverviewItem>
+              <span>Total Suply:</span>
+              <span>{priceInfo?.total_supply}</span>
+            </OverviewItem>
+            <OverviewItem>
+              <span>Max Supply:</span>
+              <span>{priceInfo?.max_supply}</span>
+            </OverviewItem>
+          </Overview>
+          
+          <Tabs>
+            <Tab isActive={chartMatch !== null }>
+              <Link to={`/${coinId}/chart`}>Chart</Link>
+            </Tab>
+            <Tab isActive={priceMatch !== null}>
+              <Link to={`/${coinId}/price`}>Price </Link>
+            </Tab>
+          </Tabs>
+
+          <Switch>
+            <Route path={`/:coinId/price`}>
+              <Price />
+            </Route>
+            <Route path={`/:coinId/chart`}>
+              <Chart />
+            </Route>
+          </Switch>
+        </>
+      )}
     </Container>
   );
-};
-
+}
 export default Coin;
