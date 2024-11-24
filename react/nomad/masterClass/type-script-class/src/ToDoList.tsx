a38f655da1078190d5c6baaf2dd91f29d7220304
ChaejinE <joung6517@gmail.com>
2024-11-24 18:37:50 +0900

    [TODOList] Custom Validation

--- react/nomad/masterClass/type-script-class/src/ToDoList.tsx
+++ react/nomad/masterClass/type-script-class/src/ToDoList.tsx
@@ -7,20 +7,24 @@ interface IForm {
     lastName: string
     username: string
     password: string
+    password1: string
+    extraError: string
 }
 
 function ToDoList() {
     // The handleSubmit func has reponsible for managing somthing like Preventing default, validation, etc...  
-    const { register, handleSubmit, formState: { errors } } = useForm<IForm>({
+    const { register, handleSubmit, formState: { errors }, setError } = useForm<IForm>({
         defaultValues: {
             email: "@naver.com"
         }
     });
     // When it's invalid, react-hook helps us modifying in terms of user-experience
-    const onValid = (data: any) => {
-        console.log(data);
+    const onValid = (data: IForm) => {
+        if (data.password != data.password1) {
+             setError("password1", { message: "Passwords are not the same" }, { shouldFocus: true  })
+        }
+         // setError("extraError", { message: "Server offline" })
     };
-    console.log(errors)
 
     return <div>
         <form style={{display: "flex", flexDirection: "column"}} onSubmit={handleSubmit(onValid)}>
@@ -34,7 +38,13 @@ function ToDoList() {
             )} placeholder="Email">
             </input>
             <span>{ errors?.email?.message as string }</span>
-            <input {...register("firstName", { required: "firstName is required" })}  placeholder="firstName"></input>
+            <input {...register("firstName", { 
+                required: "firstName is required",
+                validate: {
+                    noNico: (value) => value.includes("nico") ? "no nico allowed" : true,
+                    noNick: (value) => value.includes("nick") ? "no nick  allowed" : true  
+                },
+                })}  placeholder="firstName"></input>
             <span>{ errors?.firstName?.message as string }</span>
             <input {...register("lastName", { required: "lastName is required" })}  placeholder="lastName"></input>
             <span>{ errors?.lastName?.message as string }</span>
@@ -42,7 +52,10 @@ function ToDoList() {
             <span>{ errors?.username?.message as string }</span>
             <input {...register("password", { required: "Password is required", minLength: { value: 5, message: "Your password very short"} })}  placeholder="password"></input>
             <span>{ errors?.password?.message as string }</span>
+            <input {...register("password1", { required: "Password1 is required", minLength: { value: 5, message: "Your password very short"} })}  placeholder="password1"></input>
+            <span>{ errors?.password1?.message as string }</span>
             <button >Add</button>
+            <span>{ errors?.extraError?.message as string }</span>
         </form>
     </div>;
 }
