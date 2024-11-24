ed349eb190f574badcb3313a0bf513f0f3998685
ChaejinE <joung6517@gmail.com>
2024-11-24 17:43:23 +0900

    [TODOList] Form Vaildation

--- react/nomad/masterClass/type-script-class/src/ToDoList.tsx
+++ react/nomad/masterClass/type-script-class/src/ToDoList.tsx
@@ -1,16 +1,21 @@
 import { useForm } from "react-hook-form";
 
 function ToDoList() {
-    const { register, watch }= useForm();
-    console.log(watch());
+    // The handleSubmit func has reponsible for managing somthing like Preventing default, validation, etc...  
+    const { register, handleSubmit, formState }= useForm();
+    // When it's invalid, react-hook helps us modifying in terms of user-experience
+    const onValid = (data: any) => {
+        console.log(data);
+    };
+    console.log(formState.errors)
 
     return <div>
-        <form>
-            <input {...register("Email")}  placeholder="Email"></input>
-            <input {...register("firstName")}  placeholder="firstName"></input>
-            <input {...register("lastName")}  placeholder="lastName"></input>
-            <input {...register("username")}  placeholder="username"></input>
-            <input {...register("password")}  placeholder="password"></input>
+        <form style={{display: "flex", flexDirection: "column"}} onSubmit={handleSubmit(onValid)}>
+            <input {...register("Email", { required: true })}  placeholder="Email"></input>
+            <input {...register("firstName", { required: true })}  placeholder="firstName"></input>
+            <input {...register("lastName", { required: true })}  placeholder="lastName"></input>
+            <input {...register("username", { required: true, minLength: 10 })}  placeholder="username"></input>
+            <input {...register("password", { required: "Password is required", minLength: { value: 5, message: "Your password very short"} })}  placeholder="password"></input>
             <button >Add</button>
         </form>
     </div>;
