e367782b72fb01c11eb001c40feb80153d0d22f3
ChaejinE <joung6517@gmail.com>
2024-12-01 17:17:58 +0900

    [TODOList] Selectors part Two

--- react/nomad/masterClass/type-script-class/src/components/ToDoList.tsx
+++ react/nomad/masterClass/type-script-class/src/components/ToDoList.tsx
@@ -1,31 +1,31 @@
-import { useRecoilValue  } from "recoil";
+import { useRecoilState, useRecoilValue  } from "recoil";
 import CreateToDo from "./CreateToDo";
-import { toDoSelector } from "../atoms";
+import { categoryState, toDoSelector } from "../atoms";
 import ToDo from "./ToDo";
 
 function ToDoList() {
-    const [toDos, doing, done] = useRecoilValue(toDoSelector);
+    const toDos = useRecoilValue(toDoSelector);
+    console.log(toDos);
+    const [category, setCategory] = useRecoilState(categoryState);
+    const onInput = (e: React.FormEvent<HTMLSelectElement>) => {
+        setCategory(e.currentTarget.value);
+    }
 
-    return <div>
-        <h1>To Dos</h1>
-        <hr />
-        <CreateToDo />
-        <h2>To Do</h2>
-        <ul>
-            {toDos.map(toDo => <ToDo key={toDo.id} {...toDo} />)}
-        </ul>
-        <hr />
-        <h2>Doing</h2>
-        <ul>
-            {doing.map(toDo => <ToDo key={toDo.id} {...toDo} />)}
-        </ul>
-        <hr />
-        <h2>Done</h2>
-        <ul>
-            {done.map(toDo => <ToDo key={toDo.id} {...toDo} />)}
-        </ul>
-        <hr />
-    </div>;
+    return (
+        <div>
+            <h1>To Dos</h1>
+            <hr />
+            <form>
+                <select value={category} onInput={onInput}>
+                    <option value="TO_DO">To Do</option>
+                    <option value="DOING">Doing</option>
+                    <option value="DONE">Done</option>
+                </select>
+            </form>
+            <CreateToDo />
+            {toDos?.map(toDo => <ToDo key={toDo.id} {...toDo}/> )}
+        </div>
+    );
 }
 
 export default ToDoList;
