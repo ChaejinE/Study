05d28af782ebc9946572a23d3223665b3d7edfa8
ChaejinE <joung6517@gmail.com>
2024-12-03 17:49:28 +0900

    [TrelloClone] Settings, Get Selectors

--- react/nomad/masterClass/type-script-class/src/atoms.tsx
+++ react/nomad/masterClass/type-script-class/src/atoms.tsx
@@ -1,32 +1,14 @@
 import { atom, selector } from "recoil";
 
-export enum Categories {
-    "TO_DO" = "TO_DO",
-    "DOING" = "DOING",
-    "DONE" = "DONE"
-}
-
-export interface IToDo {
-    text: string;
-    category: Categories; // restrcit of string
-    id: number;
-}
-
-export const categoryState = atom<Categories>({
-    key: "category",
-    default: Categories.TO_DO
-})
-
-export const toDoState = atom<IToDo[]>({
-    key: "toDo", 
-    default: []
+export const minuteState = atom({
+  key: "minutes",
+  default: 0,
 });
 
-export const toDoSelector = selector({
-   key: "toDoSelector",
-   get: ({ get }) => {
-    const toDos = get(toDoState);
-    const category = get(categoryState); 
-    return toDos.filter(toDo => toDo.category === category)
-   } // It should be for getting atom
+export const hourSelector = selector({
+  key: "hours",
+  get: ({ get }) => {
+    const minutes = get(minuteState);
+    return minutes / 60;
+  },
 });
