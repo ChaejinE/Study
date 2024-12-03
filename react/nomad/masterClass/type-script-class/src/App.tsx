76fa89e2d4e880233909d4c2fac1fb9cebd2540b
ChaejinE <joung6517@gmail.com>
2024-12-03 18:47:30 +0900

    [TrelloClone] Set Selectors

--- react/nomad/masterClass/type-script-class/src/App.tsx
+++ react/nomad/masterClass/type-script-class/src/App.tsx
@@ -1,23 +1,31 @@
-import { useRecoilState, useRecoilValue } from "recoil";
+import { useRecoilState } from "recoil";
 import { hourSelector, minuteState } from "./atoms";
 
 function App() {
   const [minutes, setMinutes] = useRecoilState(minuteState);
-  const hours = useRecoilValue(hourSelector);
+  const [hours, setHours] = useRecoilState(hourSelector); // [ get element, set eleme ]
   const onMinutesChange = (event: React.FormEvent<HTMLInputElement>) => {
     setMinutes(+event.currentTarget.value); // + makes strings numbers
   };
+  const onHoursChange = (event: React.FormEvent<HTMLInputElement>) => {
+    setHours(+event.currentTarget.value); // + makes strings numbers
+  };
 
   return (
-    <>
+    <div>
       <input
         value={minutes}
         onChange={onMinutesChange}
         type="number"
         placeholder="Minutes"
       />
-      <input value={hours} type="number" placeholder="Hours" />
-    </>
+      <input
+        value={hours}
+        onChange={onHoursChange}
+        type="number"
+        placeholder="Hours"
+      />
+    </div>
   );
 }
 
