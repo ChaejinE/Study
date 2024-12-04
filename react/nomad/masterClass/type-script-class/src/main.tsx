6e1415bc30d580ede00337867e3fe90fe20228e4
ChaejinE <joung6517@gmail.com>
2024-12-04 16:31:28 +0900

    [TrelloClone] Drag and Drop

--- react/nomad/masterClass/type-script-class/src/main.tsx
+++ react/nomad/masterClass/type-script-class/src/main.tsx
@@ -80,12 +80,12 @@ const GlobalStyle = createGlobalStyle`
 
 const root = ReactDOM.createRoot(document.getElementById("root")!);
 root.render(
-  <React.StrictMode>
-    <RecoilRoot>
-      <ThemeProvider theme={darkTheme}>
-        <GlobalStyle />
-        <App />
-      </ThemeProvider>
-    </RecoilRoot>
-  </React.StrictMode>
+  // <React.StrictMode>
+  <RecoilRoot>
+    <ThemeProvider theme={darkTheme}>
+      <GlobalStyle />
+      <App />
+    </ThemeProvider>
+  </RecoilRoot>
+  // </React.StrictMode>
 );
