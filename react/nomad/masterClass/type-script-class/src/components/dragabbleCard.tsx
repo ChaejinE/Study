feb3c298be84ae244dcb08c8bfc53b8463d53d5e
ChaejinE <joung6517@gmail.com>
2024-12-04 18:42:06 +0900

    [TrelloClone] Performance

---
+++ react/nomad/masterClass/type-script-class/src/components/dragabbleCard.tsx
@@ -0,0 +1,33 @@
+import { Draggable } from "react-beautiful-dnd";
+import styled from "styled-components";
+import React from "react";
+
+const Card = styled.div`
+  border-radius: 5px;
+  padding: 5px 10px;
+  margin-bottom: 5px;
+  background-color: ${(props) => props.theme.cardColor};
+`;
+
+interface IDragabbleProps {
+  toDo: string;
+  index: number;
+}
+
+function DragabbleCard({ toDo, index }: IDragabbleProps) {
+  return (
+    <Draggable key={toDo} draggableId={toDo} index={index}>
+      {(provided) => (
+        <Card
+          ref={provided.innerRef}
+          {...provided.draggableProps}
+          {...provided.dragHandleProps}
+        >
+          {toDo}
+        </Card>
+      )}
+    </Draggable>
+  );
+}
+
+export default React.memo(DragabbleCard);
