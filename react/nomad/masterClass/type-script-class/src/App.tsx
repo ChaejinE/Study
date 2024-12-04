7ff948bfb79adb734da9afac5a1d24f3c5bf0a02
ChaejinE <joung6517@gmail.com>
2024-12-04 19:17:02 +0900

    [TrelloClone] Multi Boards

--- react/nomad/masterClass/type-script-class/src/App.tsx
+++ react/nomad/masterClass/type-script-class/src/App.tsx
@@ -1,12 +1,12 @@
-import { DragDropContext, Droppable, DropResult } from "react-beautiful-dnd";
+import { DragDropContext, DropResult } from "react-beautiful-dnd";
 import { useRecoilState } from "recoil";
 import styled from "styled-components";
 import { toDoState } from "./atoms";
-import DragabbleCard from "./components/dragabbleCard";
+import Board from "./components/Board";
 
 const Wrapper = styled.div`
   display: flex;
-  max-width: 480px;
+  max-width: 680px;
   width: 100%;
   margin: 0 auto;
   justify-content: center;
@@ -16,16 +16,9 @@ const Wrapper = styled.div`
 
 const Boards = styled.div`
   display: grid;
-  grid-template-columns: repeat(1, 1fr);
+  grid-template-columns: repeat(3, 1fr);
   width: 100%;
-`;
-
-const Board = styled.div`
-  padding: 20px 10px;
-  padding-top: 30px;
-  background-color: ${(props) => props.theme.boardColor};
-  border-radius: 5px;
-  min-height: 200px;
+  gap: 10px;
 `;
 
 function App() {
@@ -33,14 +26,13 @@ function App() {
 
   const onDragEnd = ({ draggableId, destination, source }: DropResult) => {
     setToDos((oldTodos) => {
-      if (!destination) return;
-
-      const toDosCopy = [...oldTodos];
-      // Delete item
-      toDosCopy.splice(source.index, 1);
-      // Put item to destination
-      toDosCopy.splice(destination?.index, 0, draggableId);
-      return toDosCopy; // There is some short of shape a little bit
+      // if (!destination) return;
+      // const toDosCopy = [...oldTodos];
+      // // Delete item
+      // toDosCopy.splice(source.index, 1);
+      // // Put item to destination
+      // toDosCopy.splice(destination?.index, 0, draggableId);
+      // return toDosCopy; // There is some short of shape a little bit
     });
   };
 
@@ -49,16 +41,9 @@ function App() {
     <DragDropContext onDragEnd={onDragEnd}>
       <Wrapper>
         <Boards>
-          <Droppable droppableId="one">
-            {(provided) => (
-              <Board ref={provided.innerRef} {...provided.droppableProps}>
-                {toDos.map((toDo, index) => (
-                  <DragabbleCard key={toDo} index={index} toDo={toDo} />
-                ))}
-                {provided.placeholder}
-              </Board>
-            )}
-          </Droppable>
+          {Object.keys(toDos).map((boardId) => (
+            <Board boardId={boardId} key={boardId} toDos={toDos[boardId]} />
+          ))}
         </Boards>
       </Wrapper>
     </DragDropContext>
