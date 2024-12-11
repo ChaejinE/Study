1cc9f0a627675af8b48f1f977fd0173b3600a8c4
ChaejinE <joung6517@gmail.com>
2024-12-11 16:06:54 +0900

    [TrelloClone] Final Styles

--- react/nomad/masterClass/type-script-class/src/components/dragabbleCard.tsx
+++ react/nomad/masterClass/type-script-class/src/components/dragabbleCard.tsx
@@ -2,11 +2,14 @@ import { Draggable } from "react-beautiful-dnd";
 import styled from "styled-components";
 import React from "react";
 
-const Card = styled.div`
+const Card = styled.div<{ isDragging: boolean }>`
   border-radius: 5px;
   padding: 5px 10px;
   margin-bottom: 5px;
-  background-color: ${(props) => props.theme.cardColor};
+  background-color: ${(props) =>
+    props.isDragging ? "#74b9ff" : props.theme.cardColor};
+  box-shadow: ${(props) =>
+    props.isDragging ? "0px 2px 25px rgba(0, 0, 0, 0.5)" : "none"};
 `;
 
 interface IDragabbleProps {
@@ -17,9 +20,10 @@ interface IDragabbleProps {
 function DragabbleCard({ toDo, index }: IDragabbleProps) {
   return (
     <Draggable key={toDo} draggableId={toDo} index={index}>
-      {(provided) => (
+      {(provided, snapshot) => (
         <Card
           ref={provided.innerRef}
+          isDragging={snapshot.isDragging}
           {...provided.draggableProps}
           {...provided.dragHandleProps}
         >
