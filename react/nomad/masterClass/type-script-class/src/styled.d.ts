cc144220061b05a2071ab0e8bdc7ed95ef8d0c89
ChaejinE <joung6517@gmail.com>
2024-12-13 16:28:38 +0900

    [Animations] Intro

--- react/nomad/masterClass/type-script-class/src/styled.d.ts
+++ react/nomad/masterClass/type-script-class/src/styled.d.ts
@@ -1,13 +1,5 @@
-// import original module declarations
-import 'styled-components';
+import "styled-components";
 
-
-// and extend them!
-// https://styled-components.com/docs/api#typescript
-declare module 'styled-components' {
-  export interface DefaultTheme {
-    bgColor:  string;
-    boardColor: string;
-    cardColor: string;
-  }
+declare module "styled-components" {
+  export interface DefaultTheme {}
 }
--- react/nomad/masterClass/type-script-class/src/theme.ts
+++ react/nomad/masterClass/type-script-class/src/theme.ts
@@ -1,9 +1,3 @@
-import { DefaultTheme } from "styled-components/dist/types";
+import { DefaultTheme } from "styled-components";
 
-export const darkTheme: DefaultTheme = {
-  /* Reference Color Site : https://flatuicolors.com/palette/gb */
-  /* U should modify the interface in the styled.d.ts */
-  bgColor: "#3F8CF2",
-  boardColor: "#E6EAEE",
-  cardColor: "white",
-};
+export const darkTheme: DefaultTheme = {};
