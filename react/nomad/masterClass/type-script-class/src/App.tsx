f9d214619b8afb0f0aeccc7d8187f47e12c53b98
ChaejinE <joung6517@gmail.com>
2024-12-17 21:50:04 +0900

    [Animations] Guestures - drag

--- react/nomad/masterClass/type-script-class/src/App.tsx
+++ react/nomad/masterClass/type-script-class/src/App.tsx
@@ -1,5 +1,6 @@
 import styled from "styled-components";
 import { motion, Variants } from "motion/react";
+import { useRef } from "react";
 
 const Wrapper = styled.div`
   height: 100vh;
@@ -7,6 +8,17 @@ const Wrapper = styled.div`
   display: flex;
   justify-content: center;
   align-items: center;
+`;
+
+const BiggerBox = styled(motion.div)`
+  width: 600px;
+  height: 600px;
+  background-color: rgba(255, 255, 255, 0.4);
+  border-radius: 40px;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  /* overflow: hidden; */
 `;
 
 const Box = styled(motion.div)`
@@ -20,13 +32,32 @@ const Box = styled(motion.div)`
 const boxVariants: Variants = {
   hover: { scale: 1.5, rotateZ: 90 },
   click: { scale: 1, borderRadius: "100px" },
+  drag: {
+    backgroundColor: "rgb(46, 204, 113)",
+    transition: {
+      duration: 10,
+    },
+  },
 };
 
 function App() {
+  const biggerBoxRef = useRef<HTMLDivElement>(null);
+
   // If u use right props, u can do anything !
   return (
     <Wrapper>
-      <Box variants={boxVariants} whileHover="hover" whileTap="click" />
+      <BiggerBox ref={biggerBoxRef}>
+        <Box
+          drag // we can fix its axis by setting darg="x" or "y"
+          dragSnapToOrigin
+          dragElastic={0.5}
+          dragConstraints={biggerBoxRef}
+          variants={boxVariants}
+          whileHover="hover"
+          whileTap="click"
+          whileDrag="drag" // It should be rgb, if u not use color string
+        />
+      </BiggerBox>
     </Wrapper>
   );
 }
