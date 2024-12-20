17b8e3b116e6e0b4ce7f4158b07eeb908851d3e7
ChaejinE <joung6517@gmail.com>
2024-12-20 18:45:06 +0900

    [Nomplix] Header Part One

--- react/nomad/masterClass/nomflix/src/App.tsx
+++ react/nomad/masterClass/nomflix/src/App.tsx
@@ -9,15 +9,9 @@ function App() {
     <BrowserRouter>
       <Header />
       <Routes>
-        <Route path="/">
-          <Home />
-        </Route>
-        <Route path="/tv">
-          <Tv />
-        </Route>
-        <Route path="/search">
-          <Search />
-        </Route>
+        <Route path="/" element={<Home />}></Route>
+        <Route path="/tv" element={<Tv />}></Route>
+        <Route path="/search" element={<Search />}></Route>
       </Routes>
     </BrowserRouter>
   );
